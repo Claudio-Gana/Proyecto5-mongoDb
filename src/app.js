2bcfc3512fcf9a6318a0522be07ecbe51c5682aa
chamuh8 <115745993+chamuh8@users.noreply.github.com>
2023-07-14 00:10:57 -0400

    comm

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use( express.json() )
 app.use( cors() )
 
 app.get( '/', function ( req, res ) {
-  return res.status( 200 ).json( { msg: "Bienvenido" } )
+  return res.status( 200 ).json( { msg: "Bienvenido esto funciona" } )
 } )
 
 app.use( usersRouter )
