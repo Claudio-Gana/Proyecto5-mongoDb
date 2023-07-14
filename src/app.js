a952676682ba1bbf43c6cbbe952869a04e3f89f6
chamuh8 <115745993+chamuh8@users.noreply.github.com>
2023-07-14 00:12:57 -0400

    b

--- src/app.js
+++ src/app.js
@@ -24,5 +24,5 @@ app.use( authRoutes )
 
 const { PORT } = environment
 app.listen( PORT, () => {
-  console.log( `Aplicación iniciada puerto ${ PORT }` )
+  console.log( `Aplicación iniciada en puerto ${ PORT }` )
 } )
